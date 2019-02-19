088aaa1a4496672e6ba0e88bce3f9fb10bea03c4
Tareq <tjarwan@atypon.com>
2019-02-19 14:16:33 +0200

    4th commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import classes from "./Layout.css";
 import Aux from "../../hoc/Aux";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-import classes from './Layout.css';
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    };
 
-const layout = (props) => {
-    return <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-};
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    };
+
+    render() {
+        return <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer
+                open={this.state.showSideDrawer}
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+    }
+}
+
+export default Layout;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,8 @@ const modal = (props) => (
             clicked={props.modalClosed}/>
         <div className={classes.Modal} style={{
             transform: props.show ? 'transformY(0)' : 'transformY(-100vh)',
-            opacity: props.show ? '1' : '0'
+            opacity: props.show ? '1' : '0',
+            visibility: props.show ? 'visible' : 'hidden'
         }}>
             {props.children}
         </div>
