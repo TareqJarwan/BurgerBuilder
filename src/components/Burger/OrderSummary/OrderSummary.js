7af5e8802c2f9c642dfcc9100fc3a73d737bec97
Tareq <tjarwan@atypon.com>
2019-02-19 15:02:33 +0200

    4th commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
-    const ingredientSumary = Object.keys(props.ingredients).map((item) => {
+    const ingredientSummary = Object.keys(props.ingredients).map((item) => {
         return (
             <li key={item}>{item.charAt(0).toUpperCase() + item.slice(1)}: {props.ingredients[item]}</li>
         )
@@ -13,7 +13,7 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-                {ingredientSumary}
+                {ingredientSummary}
             </ul>
             <p><strong>Total: {props.price.toFixed(2)}$</strong></p>
             <p>Continue to Checkout?</p>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 
 import classes from './Modal.css';
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
 
 class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         return nextProps.show !== this.props.show;
-    }
-
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        console.log('[Modal] Will Update')
     }
 
     render() {
